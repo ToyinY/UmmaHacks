b23937beb8afa4de2e576c7b800269501ee65fc6
ToyinY <43014725+ToyinY@users.noreply.github.com>
2020-07-14 21:29:02 -0400

    Adding c# 2d mmo project

---
+++ LibraryInterface/LibraryInterface/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace LibraryInterface
+{
+    class Program
+    {
+        public static RenderWindow window;
+        
+        static void Main(string[] args)
+        {
+            window = new RenderWindow(new SFML.Window.VideoMode(800, 600), "Library");
+            Game();
+        }
+
+        static void Game()
+        {
+            while(window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear(Color.Red);
+                window.Display();
+            }
+        }
+    }
+}
