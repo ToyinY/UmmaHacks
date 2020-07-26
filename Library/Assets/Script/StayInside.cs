98bdfa83819bab72c77bbcc1772701749bc357c6
ToyinY <43014725+ToyinY@users.noreply.github.com>
2020-07-26 18:56:39 -0400

    Moving website game code

---
+++ Library/Assets/Script/StayInside.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class StayInside : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2f, 2f), 
+            Mathf.Clamp(transform.position.y, -4f, 4f), transform.position.z);
+    }
+}
